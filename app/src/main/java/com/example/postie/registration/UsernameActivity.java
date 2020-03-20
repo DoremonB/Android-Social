eaf491ba03859a3c79efdd790e8453ab20c35dd2
Rugved <rugved.bongale@somaiya.edu>
2020-03-20 12:21:26 +0530

    Added 3 important libraries ...circle image,crop image,glide to show image...and imlemented it successfully....username file consists of github library links

--- app/src/main/java/com/example/postie/registration/UsernameActivity.java
+++ app/src/main/java/com/example/postie/registration/UsernameActivity.java
@@ -2,15 +2,103 @@ package com.example.postie.registration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.postie.R;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UsernameActivity extends AppCompatActivity {
+
+    private CircleImageView profileImageView;
+    private Button removeBtn,createAccountBtn;
+    private EditText username;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_username);
+
+        init();
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withActivity(UsernameActivity.this)
+                        .withPermissions(
+                                Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ).withListener(new MultiplePermissionsListener() {
+                    @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if(report.areAllPermissionsGranted()){
+                            selectImage();            
+                        }
+                        else{
+                            Toast.makeText(UsernameActivity.this, "Please allow Permissions", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {/* ... */}
+                }).check();
+                
+            }
+        });
+    }
+    private void init(){
+        profileImageView=findViewById(R.id.profile_image);
+        removeBtn=findViewById(R.id.remove_btn);
+        createAccountBtn=findViewById(R.id.create_account_btn);
+        progressBar=findViewById(R.id.progressBar);
+        username=findViewById(R.id.username);
+    }
+
+    private void selectImage(){
+        // start picker to get image for cropping and then use the image in cropping activity
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setActivityMenuIconColor(getResources().getColor(R.color.colorAccent))
+                .setActivityTitle("Profile Photo")
+                .setFixAspectRatio(true)
+                .setAspectRatio(1,1)
+                .start(this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri photoUri = result.getUri();
+
+                Glide
+                    .with(this)
+                    .load(photoUri)
+                    .centerCrop()
+                    .placeholder(R.drawable.default_profile_pic)
+                    .into(profileImageView);
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Toast.makeText(this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
