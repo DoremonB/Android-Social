80b9225d7a2bcaa5b87f1b2d758937a06abe017d
Rugved <rugved.bongale@somaiya.edu>
2020-03-20 22:22:13 +0530

    Created layout for story and created its adapter

---
+++ app/src/main/java/com/example/postie/Adapters/StoriesAdapter.java
@@ -0,0 +1,62 @@
+package com.example.postie.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.postie.Models.StoryModel;
+import com.example.postie.R;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class StoriesAdapter extends RecyclerView.Adapter<StoriesAdapter.StoryViewHolder> {
+
+    private List<StoryModel> list;
+
+    public StoriesAdapter(List<StoryModel> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public StoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.story_item,parent,false);
+
+        return new StoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StoryViewHolder holder, int position) {
+
+        Glide
+                .with(holder.itemView.getContext())
+                .load(list.get(position).getImages().get(0))
+                .placeholder(R.drawable.default_profile_pic)
+                .into(holder.thumnail);
+        holder.name.setText(list.get(position).getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class StoryViewHolder extends  RecyclerView.ViewHolder{
+
+        private CircleImageView thumnail;
+        private TextView name;
+        public StoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            thumnail=itemView.findViewById(R.id.story);
+            name=itemView.findViewById(R.id.name);
+        }
+    }
+}
--- app/src/main/java/com/example/postie/fragments/HomeFragment.java
+++ app/src/main/java/com/example/postie/fragments/HomeFragment.java
@@ -2,13 +2,22 @@ package com.example.postie.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.postie.Adapters.StoriesAdapter;
+import com.example.postie.Models.StoryModel;
 import com.example.postie.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +27,9 @@ public class HomeFragment extends Fragment {
     public HomeFragment() {
         // Required empty public constructor
     }
+    private RecyclerView stories,feeds;
+    private StoriesAdapter storiesAdapter;
+
 
 
     @Override
@@ -26,4 +38,43 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        init(view);
+
+        LinearLayoutManager storiesLayoutManager=new LinearLayoutManager(getContext());
+        storiesLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        stories.setLayoutManager(storiesLayoutManager);
+
+        LinearLayoutManager feedsLayoutManager=new LinearLayoutManager(getContext());
+        feedsLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        feeds.setLayoutManager(feedsLayoutManager);
+
+        List<String> images=new ArrayList<>();
+        images.add("");
+
+        List<StoryModel> list=new ArrayList<>();
+        list.add(new StoryModel(images,"John Doe"));
+        list.add(new StoryModel(images,"John Doe"));
+        list.add(new StoryModel(images,"John Doe"));
+        list.add(new StoryModel(images,"Johasasasasasasasasasasasn Doe"));
+        list.add(new StoryModel(images,"John Doe"));
+        list.add(new StoryModel(images,"John Doe"));
+        list.add(new StoryModel(images,"John Doe"));
+        list.add(new StoryModel(images,"John Doe"));
+
+        storiesAdapter=new StoriesAdapter(list);
+        stories.setAdapter(storiesAdapter);
+
+    }
+
+    private void init(View view){
+        stories=view.findViewById(R.id.stories);
+        feeds=view.findViewById(R.id.feeds);
+
+    }
 }
+
