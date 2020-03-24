86944b174ce4aac35927345cb27dad126749edfc
Rugved <rugved.bongale@somaiya.edu>
2020-03-24 13:12:46 +0530

    touch listeners added to story ...stories working properly

--- app/src/main/java/com/example/postie/StoryActivity.java
+++ app/src/main/java/com/example/postie/StoryActivity.java
@@ -3,6 +3,10 @@ package com.example.postie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -21,6 +25,9 @@ public class StoryActivity extends AppCompatActivity {
     private LinearLayout progressContainer;
     public static List<String> images;
     private ProgressBar defaultProgressBar;
+    private boolean timerON=true;
+    private float upperLimit;
+    private float lowerLimit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,55 @@ public class StoryActivity extends AppCompatActivity {
         init();
         Glide.with(this).load(images.get(progressBarIndex)).placeholder(R.drawable.default_profile_pic).into(imageView);
         setProgressBars();
+        setControls();
+        setTimer();
+
+    }
+
+    private void setControls() {
+
+        float widthProportion=(getResources().getDisplayMetrics().widthPixels/100.0f)*25;
+
+        upperLimit=getResources().getDisplayMetrics().widthPixels-widthProportion;
+        lowerLimit=widthProportion;
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+
+                        if(event.getX()<=lowerLimit && progressBarIndex>0){
+                            ((ProgressBar) progressContainer.getChildAt(progressBarIndex))
+                                    .setProgress(0);
+                            progressBarIndex--;
+                            ((ProgressBar) progressContainer.getChildAt(progressBarIndex))
+                                    .setProgress(0);
+                            progressCount=0;
+                            Glide.with(getApplicationContext()).load(images.get(progressBarIndex)).into(imageView);
+                        }
+                        else if(event.getX()>=upperLimit && progressBarIndex<progressContainer.getChildCount()-1){
+                            ((ProgressBar) progressContainer.getChildAt(progressBarIndex))
+                                    .setProgress(100);
+                            progressBarIndex++;
+                            ((ProgressBar) progressContainer.getChildAt(progressBarIndex))
+                                    .setProgress(0);
+                            progressCount=0;
+                            Glide.with(getApplicationContext()).load(images.get(progressBarIndex)).into(imageView);
+                        }
+                        else {
+                            timerON = false;
+                            return true;
+                        }
+                    case MotionEvent.ACTION_UP:
+                        timerON=true;
+                        return true;
+                }
+
+                return false;
+            }
+        });
 
     }
 
@@ -69,30 +125,29 @@ public class StoryActivity extends AppCompatActivity {
 //                1000ms * 1 = 1s //will be called 1 time in 1 sec
 //                100ms * 10 = 1s //will be called 10 times in 1 sec
 //                50ms * 100 =5s
+                    if (timerON){
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (progressCount == 100) {
+                                    progressBarIndex++;
+                                    if (progressBarIndex < progressContainer.getChildCount()) {
+                                        progressCount = 0;
+                                        //Change Image Here
+                                        Glide.with(getApplicationContext()).load(images.get(progressBarIndex)).into(imageView);
+                                    } else {
+                                        finish();
+                                    }
 
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if(progressCount == 100){
-                            progressBarIndex++;
-                            if(progressBarIndex<progressContainer.getChildCount()){
-                                progressCount=0;
-                                //Change Image Here
-                                Glide.with(getApplicationContext()).load(images.get(progressBarIndex)).into(imageView);
+
+                                } else {
+                                    progressCount++;
+                                    ((ProgressBar) progressContainer.getChildAt(progressBarIndex))
+                                            .setProgress(progressCount);
+                                }
                             }
-                            else{
-                                finish();
-                            }
-
-
-
-                        }else {
-                            progressCount++;
-                            ((ProgressBar) progressContainer.getChildAt(progressBarIndex))
-                                    .setProgress(progressCount);
-                        }
+                        });
                     }
-                });
 
             }
         },0,50);
